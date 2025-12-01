13b8e1320bd7f2811c0b2b752469f80bf3e3fd8f
VioletMondlicht <160015434+VioletMondlicht@users.noreply.github.com>
2025-12-01 23:51:26 +0100

    add PuzzleSolver logic

--- day1/day1/PuzzleSolver.cs
+++ day1/day1/PuzzleSolver.cs
@@ -2,8 +2,40 @@ namespace day1;
 
 public class PuzzleSolver
 {
-    public int start = 50;
+    public int currentPosition = 50;
+    public int password = 0;
     public void SolvePuzzle()
+    {
+        Console.WriteLine("Reading input.txt, please wait...");
+        string[] input = File.ReadAllLines("input.txt");
+
+    foreach(string line in input)
         {
+            if (line[0] == 'L')
+            {
+                int.TryParse(line.Substring(1), out int command);
+                
+                int newCurrentPosition = currentPosition - command;
+                if (newCurrentPosition < 0)
+                {
+                    newCurrentPosition = newCurrentPosition+100;
+                }
+                currentPosition = newCurrentPosition;
+            }
+            if (line[0] == 'R')
+            {
+                int.TryParse(line.Substring(1), out int command);
+
+                int newCurrentPosition = (currentPosition+command)%100;
+                currentPosition = newCurrentPosition;
+            }
+
+            if (currentPosition == 0)
+                {
+                    password++;
+                }
         }
+    Console.WriteLine($"{password}");
+
+    }
 }
