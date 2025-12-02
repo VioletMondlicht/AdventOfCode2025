7baf1baf5fa52c139406a7613c2547ac9964fb93
VioletMondlicht <160015434+VioletMondlicht@users.noreply.github.com>
2025-12-02 13:57:19 +0100

    call input check and idSolver

--- day2/day2/Program.cs
+++ day2/day2/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿namespace day2;
+
+public class Programm
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Heyho and welcome to the gift shop!");
+        Console.WriteLine("This is the Product-ID-Checker by VioletMondlicht");
+        Console.WriteLine("https://github.com/VioletMondlicht");
+        Console.WriteLine("Robot will check if input.txt exist, please wait...");
+        Console.WriteLine("");
+
+        CheckTxt check = new();
+        check.CheckFile();
+
+        ProductIdSolver checkID = new();
+        checkID.IdSolver();
+    }
+}
